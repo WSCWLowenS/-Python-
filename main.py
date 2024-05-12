b505319187fa418d59bd67cd08c1b2694ccbb23d
WSCWLowenS <159732836+WSCWLowenS@users.noreply.github.com>
2024-05-13 11:56:17 +1200

    h

--- main.py
+++ main.py
@@ -4,22 +4,21 @@ print ("Hello")
 name= input("What is your name?")
 #Greeting the User and introducing the quiz
 print("Welcome to the Quiz", name)
-print("This quiz is about General Knowledge")
+print("This quiz is a 10 question quiz and it's about General Knowledge")
 print("Write the capital letter of the correct answer")
 #Asking the user a question
-answer=input("What is the chemical symbol for Gold? \n A.Gd B.Go C.Ag D.Au \n Answer:")
+
+questionanswer=input("What is the chemical symbol for Gold? \nA.Gd B.Go C.Ag D.Au \nAnswer:")
 #Telling the user the correct answer
 if answer == " D":
     print("Correct!")
+    score += 100
 elif answer == "":
     print("Hahaha")
     print("The answer is D")
 else:
     print("Wrong answer")
     print("The answer is D")
-
-if answer == " D":
-    score =+ 100
 #Ending the Quiz
 print("Your final score is", score)
 print("Thank you for answering the quiz")
